a4a81ceacd3056311e2e9723e314ba8acc7aadbb
DionizijMalacko <57137277+DionizijMalacko@users.noreply.github.com>
2020-11-16 18:05:43 +0100

    Ubacivanje backend-a

---
+++ backend/src/main/java/com/example/ISA2020/Isa2020Application.java
@@ -0,0 +1,13 @@
+package com.example.ISA2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Isa2020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Isa2020Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/ISA2020/Isa2020ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ISA2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Isa2020ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
