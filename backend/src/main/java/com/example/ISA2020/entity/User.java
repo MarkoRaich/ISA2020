12b01e7667d670ba37fdedb5615089d9c1e22ec1
DionizijMalacko <57137277+DionizijMalacko@users.noreply.github.com>
2020-11-17 00:34:39 +0100

    Postavljanje MySql baze

---
+++ backend/src/main/java/com/example/ISA2020/entity/User.java
@@ -0,0 +1,41 @@
+package com.example.ISA2020.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(columnDefinition = "VARCHAR(50)", nullable = false)
+	private String name;
+	
+	public User() {
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+}
