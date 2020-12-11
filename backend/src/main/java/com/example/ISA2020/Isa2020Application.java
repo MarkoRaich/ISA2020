467df170fa3dbcd306fc493505867b2f272ad839
DionizijMalacko <57137277+DionizijMalacko@users.noreply.github.com>
2020-12-11 01:35:59 +0100

    Implementacija Upload i Download Fajlova preko Http-a.

--- backend/src/main/java/com/example/ISA2020/Isa2020Application.java
+++ backend/src/main/java/com/example/ISA2020/Isa2020Application.java
@@ -4,18 +4,24 @@ import java.io.IOException;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+import com.example.ISA2020.files.FileStorageProperties;
 import com.example.ISA2020.service.DrugAvailibilityService;
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+    FileStorageProperties.class
+})
 public class Isa2020Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Isa2020Application.class, args);
 		
+		//za gRPC
 		Server server = ServerBuilder.forPort(30051).addService(new DrugAvailibilityService()).build();
 		try {
 			server.start();
---
+++ backend/src/main/java/com/example/ISA2020/files/FileStorageException.java
@@ -0,0 +1,17 @@
+package com.example.ISA2020.files;
+
+public class FileStorageException extends RuntimeException {
+    
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public FileStorageException(String message) {
+        super(message);
+    }
+
+    public FileStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
