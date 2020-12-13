619d4b09dff2bf37bd7b5e708893369963339e2a
DionizijMalacko <57137277+DionizijMalacko@users.noreply.github.com>
2020-12-13 19:51:53 +0100

    Popunjena baza sa vrednostima za drug i pharmacy.

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -7,3 +7,13 @@ INSERT INTO authority (name) VALUES ('ROLE_NORMAL_USER');
 INSERT INTO normal_user (username, password, first_name, last_name, email, country, phone_number, address, city)
 VALUES ('user1', 'password', 'User1', 'Useric1', 'user1@gmail.com', 'Serbia', '+38112345678', 'Neka Adresa St. 1', 'Novi Sad');
 INSERT INTO normal_user_authority (normal_user_id, authority_id) VALUES (1, 1);
+
+INSERT INTO drug (id, code, name) VALUES (1, '20022', 'Brufen');
+INSERT INTO drug (id, code, name) VALUES (2, '20033', 'Brufen');
+INSERT INTO drug (id, code, name) VALUES (3, '20044', 'Brufen');
+INSERT INTO drug (id, code, name) VALUES (4, '20055', 'Brufen');
+INSERT INTO drug (id, code, name) VALUES (5, '20066', 'Brufen');
+
+INSERT INTO pharmacy (id, api_key, name) values (1, '123ssdasda', 'Jankovic');
+INSERT INTO pharmacy (id, api_key, name) values (2, '4654saddas', 'Zegin');
+INSERT INTO pharmacy (id, api_key, name) values (3, '787qwqeqqq', 'Benu');
