8f1e22c24bb2fb78fcfb6d23d71364e398913fb7
DionizijMalacko <57137277+DionizijMalacko@users.noreply.github.com>
2020-12-13 20:31:21 +0100

    Uradjena metoda sa MultiPart responsom. Bilo je potrebno slanje
    Bilo je potrebno slanje id leka i njegove specifikacije u vidu fajla.

--- backend/src/main/java/com/example/ISA2020/controller/DrugController.java
+++ backend/src/main/java/com/example/ISA2020/controller/DrugController.java
@@ -7,13 +7,17 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.ISA2020.dto.DrugDTO;
@@ -135,5 +139,67 @@ public class DrugController {
 			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 		}
 		return new ResponseEntity<>(drugDTO, HttpStatus.OK);
+	}
+	
+	@RequestMapping(method = { RequestMethod.GET },value = "/multipartdata/getByCode/{code}", produces=MediaType.MULTIPART_FORM_DATA_VALUE)
+	public ResponseEntity<MultiValueMap<String, Object>> getFileByCode(@PathVariable String code) throws IOException{
+		Drug drug = drugService.findByCode(code);
+		if(drug == null) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+		
+		File file = new File("./Resources/response" + code + ".txt"); 
+		  
+		//Create the file
+		if (file.createNewFile())
+		{
+		    System.out.println("File is created!");
+		} else {
+		    System.out.println("File already exists.");
+		}
+		
+		//Write Content
+		FileWriter writer = new FileWriter(file);
+		writer.write("Sifra leka: " + drug.getCode());
+		writer.write(System.getProperty( "line.separator" ));
+		writer.write("Ime leka: " + drug.getName());
+		writer.write(System.getProperty( "line.separator" ));
+		writer.close();
+		
+		MultiValueMap<String, Object> formData = new LinkedMultiValueMap<String, Object>();
+		formData.add(drug.getCode(),  file);
+		
+		return new ResponseEntity<MultiValueMap<String, Object>>(formData, HttpStatus.OK);
+	}
+	
+	@RequestMapping(method = { RequestMethod.GET },value = "/multipartdata/getById/{id}", produces=MediaType.MULTIPART_FORM_DATA_VALUE)
+	public ResponseEntity<MultiValueMap<String, Object>> getFileById(@PathVariable Long id) throws IOException{
+		Drug drug = drugService.findById(id);
+		if(drug == null) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+		
+		File file = new File("./Resources/response" + id + ".txt"); 
+		  
+		//Create the file
+		if (file.createNewFile())
+		{
+		    System.out.println("File is created!");
+		} else {
+		    System.out.println("File already exists.");
+		}
+		
+		//Write Content
+		FileWriter writer = new FileWriter(file);
+		writer.write("Sifra leka: " + drug.getCode());
+		writer.write(System.getProperty( "line.separator" ));
+		writer.write("Ime leka: " + drug.getName());
+		writer.write(System.getProperty( "line.separator" ));
+		writer.close();
+		
+		MultiValueMap<String, Object> formData = new LinkedMultiValueMap<String, Object>();
+		formData.add(drug.getCode(),  file);
+		
+		return new ResponseEntity<MultiValueMap<String, Object>>(formData, HttpStatus.OK);
 	}
 }
