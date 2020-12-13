381fc4aace9f56e2aef8cc494c6d2d045cf12008
DionizijMalacko <57137277+DionizijMalacko@users.noreply.github.com>
2020-12-13 17:38:16 +0100

    Uradjeno kreiranje fajla sa informacijama o leku, ili informacijama o svim lekovima u sistemu.

--- backend/src/main/java/com/example/ISA2020/controller/DrugController.java
+++ backend/src/main/java/com/example/ISA2020/controller/DrugController.java
@@ -1,5 +1,8 @@
 package com.example.ISA2020.controller;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,27 +44,86 @@ public class DrugController {
     }
 	
 	@GetMapping(value="/getAll")
-	public ResponseEntity<List<Drug>> getAllDrugs() {
+	public ResponseEntity<List<Drug>> getAllDrugs() throws IOException {
 		List<Drug> drugs = drugService.getAllDrugs();
-	
+		
+		File file = new File("c://Users//dioni//OneDrive//Desktop//Fajlovi//response" + "AllDrugs" + ".txt"); //C:\Users\dioni\OneDrive\Desktop
+		  
+		//Create the file
+		if (file.createNewFile())
+		{
+		    System.out.println("File is created!");
+		} else {
+		    System.out.println("File already exists.");
+		}
+		
+		//Write Content
+		FileWriter writer = new FileWriter(file);
+		for(Drug d : drugs) {
+			writer.write("Sifra leka: " + d.getCode());
+			writer.write(System.getProperty( "line.separator" ));
+			writer.write("Ime leka: " + d.getName());
+			writer.write(System.getProperty( "line.separator" ));
+			writer.write(System.getProperty( "line.separator" ));
+		}
+		writer.close();
+		
 		return new ResponseEntity<>(drugs, HttpStatus.OK);
 	}
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Drug> getOneById(@PathVariable Long id){
+	public ResponseEntity<Drug> getOneById(@PathVariable Long id) throws Exception{
 		Drug drug = drugService.findById(id);
 		if(drug == null) {
 			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 		}
+		
+		File file = new File("c://Users//dioni//OneDrive//Desktop//Fajlovi//response" + id + ".txt"); //C:\Users\dioni\OneDrive\Desktop
+		  
+		//Create the file
+		if (file.createNewFile())
+		{
+		    System.out.println("File is created!");
+		} else {
+		    System.out.println("File already exists.");
+		}
+		
+		//Write Content
+		FileWriter writer = new FileWriter(file);
+		writer.write("Sifra leka: " + drug.getCode());
+		writer.write(System.getProperty( "line.separator" ));
+		writer.write("Ime leka: " + drug.getName());
+		writer.write(System.getProperty( "line.separator" ));
+		writer.close();
+		
 		return new ResponseEntity<>(drug, HttpStatus.OK);
 	}
 	
 	@GetMapping(value = "/getByCode/{code}")
-	public ResponseEntity<Drug> getOneByCode(@PathVariable String code){
+	public ResponseEntity<Drug> getOneByCode(@PathVariable String code) throws IOException{
 		Drug drug = drugService.findByCode(code);
 		if(drug == null) {
 			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 		}
+		
+		File file = new File("c://Users//dioni//OneDrive//Desktop//Fajlovi//response" + code + ".txt"); 
+		  
+		//Create the file
+		if (file.createNewFile())
+		{
+		    System.out.println("File is created!");
+		} else {
+		    System.out.println("File already exists.");
+		}
+		
+		//Write Content
+		FileWriter writer = new FileWriter(file);
+		writer.write("Sifra leka: " + drug.getCode());
+		writer.write(System.getProperty( "line.separator" ));
+		writer.write("Ime leka: " + drug.getName());
+		writer.write(System.getProperty( "line.separator" ));
+		writer.close();
+		
 		return new ResponseEntity<>(drug, HttpStatus.OK);
 	}
 	
